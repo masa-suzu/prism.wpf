62be74f6f9d13e0859a16a6ac481271d283eeaf6
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-02-14 02:21:18 +0900

    init git

---
+++ src/wpf/app/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Ioc;
+using app.Views;
+using System.Windows;
+using Prism.Modularity;
+
+namespace app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<Wpf.Modules.Module1>();
+        }
+    }
+}
---
+++ src/wpf/module1/Module1.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Wpf.Modules
+{
+    public class Module1 : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(Wpf.Views.ViewA));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
